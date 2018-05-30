94003ac9c7387dcbd433c985e610f5210fe53616
santosjn <jeansjcsp@gmail.com>
2018-05-29 23:24:51 -0300

    Adjusting backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,23 +1,32 @@
 
-import React from 'react';
+import React, {Component} from 'react';
 import CustomWrapper from '../../hoc/CustomWrapper';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
-const layout = ( props ) => {
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  render() {
     return(
-        <CustomWrapper>
+      <CustomWrapper>
           <Toolbar />
-          <SideDrawer />
+          <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
           <main className={classes.Content}>
-            {props.children}
+            {this.props.children}
           </main>
-        </CustomWrapper>        
+        </CustomWrapper>   
     );
+  }
 
-};
+} 
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,22 +3,32 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import CustomWrapper from '../../../hoc/CustomWrapper'
 
 const sideDrawer = (props) => {
 
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];  
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-           
-            {/* <Logo height="11%"/> */}
+        <CustomWrapper>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+            
+                {/* <Logo height="11%"/> */}
 
-            <div className={classes.Logo}>
-                <Logo />
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </CustomWrapper>
     );
 };
 
